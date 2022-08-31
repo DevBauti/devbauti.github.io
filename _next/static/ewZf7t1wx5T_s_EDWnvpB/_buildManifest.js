db26422d71a5d14b697f67088cdd7f3c5309d1cb
Bautista Suarez <devbautisuarez@gmail.com>
2022-08-31 00:11:29 -0300

    pipo

--- _next/static/ewZf7t1wx5T_s_EDWnvpB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3fd2f2802bd358f4.css","static/chunks/pages/index-7a7f09f4dd58f3da.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
