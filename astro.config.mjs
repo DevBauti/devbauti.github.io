2a755f2eda9eca16c3af16cd28c165828c0eec0a
Bautista Suarez <devbautisuarez@gmail.com>
2022-10-27 20:31:47 -0300

    c

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://devbauti.github.io',
-    base: '/devbauti.github.io'
+    base: ''
 });
